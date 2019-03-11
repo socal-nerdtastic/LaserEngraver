e708bec2fb8d965a07f973faee3ccc3986aff818
socal-nerdtastic <37753609+socal-nerdtastic@users.noreply.github.com>
2019-03-10 22:52:07 -0700

    Add files via upload

--- stream.py
+++ stream.py
@@ -7,17 +7,24 @@ import threading
 RX_BUFFER_LINES = 10
 VERBOSE = True
 
+class FakeSerial:
+    def __init__(self, port, speed):
+        pass
+    def write(self, text):
+        pass
+    def readline(self):
+        return ""
+    def flushInput(self):
+        pass
+
+# debug line
+serial.Serial = FakeSerial
+
 class GrblControl:
     def __init__(self, port):
         self.port = port
         self.filename = None
         self.ser = serial.Serial(port, 115200)
-        # Wake up grbl
-        print("Initializing grbl...")
-        s.write(b"\r\n\r\n")
-        # Wait for grbl to initialize and flush startup text in serial input
-        time.sleep(2)
-        s.flushInput()
 
     def start(self, filename, callback=None):
         self.t = threading.Thread(target=self._run, args=(filename,callback))
@@ -30,8 +37,18 @@ class GrblControl:
 
     def _run(self, filename, callback):
         self._running = True
-        with open(filename, 'rb') as f:
-            data = f.readlines()
+        try:
+            with open(filename, 'rb') as f:
+                data = f.readlines()
+        except:
+            data = ['']*30
+
+        # Wake up grbl
+        print("Initializing grbl...")
+        self.ser.write(b"\r\n\r\n")
+        # Wait for grbl to initialize and flush startup text in serial input
+        time.sleep(2)
+        self.ser.flushInput()
 
         self.current_line = 0
         self.total_lines = len(data)
@@ -39,21 +56,21 @@ class GrblControl:
 
         # send the first lines without waiting for response
         for _ in range(RX_BUFFER_LINES):
-            ser.write(next(f))
+            self.ser.write(next(f))
         self.current_line = RX_BUFFER_LINES
 
         # send the remaining lines only after response from grbl
         for line in f:
-            s.readline() # blocks
-            if self._abort:
+            self.ser.readline() # blocks
+            if not self._running:
                 return self.stop()
-            ser.write(line)
+            self.ser.write(line)
             self.current_line += 1
 
         # read the last responses
         for _ in range(RX_BUFFER_LINES):
-            ser.readline()
-            if self._abort:
+            self.ser.readline()
+            if not self._running:
                 return self.stop()
         self.current_line += RX_BUFFER_LINES
 
